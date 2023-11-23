aa76093d4933123a8e54d629bfad7cc4423d0ecc
JoshuaIsADev <JoshuaIsADev@gmail.com>
2023-11-22 20:38:07 -0800

    updated nav menu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Drawer,
@@ -19,19 +19,70 @@ import {
 import { ShopContext } from '../context/shopContext';
 
 const NavMenu = () => {
-  const { isMenuOpen, closeMenu } = useContext(ShopContext);
+  const { isMenuOpen, closeMenu, products, fetchAllProducts } =
+    useContext(ShopContext);
+  useEffect(() => {
+    fetchAllProducts();
+  }, [fetchAllProducts]);
+
+  if (!products) return <div>Loading...</div>;
 
   return (
-    <Drawer isOpen={isMenuOpen} onClose={closeMenu} placement='left' size='sm'>
+    <Drawer isOpen={isMenuOpen} onClose={closeMenu} placement='top' size='sm'>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerCloseButton />
-          <DrawerHeader>Menu</DrawerHeader>
+          <DrawerHeader></DrawerHeader>
           <DrawerBody>
             <VStack p='2rem'>
-              <Link to='/'>About Us</Link>
-              <Link to='/'>Learn More</Link>
-              <Link to='/'>Sustainability</Link>
+              <p>Pre-Built PCs</p>
+              {products
+                .filter((product) => product.productType === 'computer')
+                .map((product) => (
+                  <Button
+                    m='0'
+                    p='0 3rem'
+                    border='none'
+                    borderRadius='0'
+                    backgroundColor='rgba(255,255,255,1)'
+                    _hover={{
+                      bg: 'rgba(0,0,0,1)',
+                      color: 'rgba(255,255,255,1)',
+                    }}
+                    onClick={closeMenu}
+                    fontSize='2.4rem'
+                    fontWeight='100'
+                    textTransform='uppercase'
+                  >
+                    <Link to={`/products/${product.handle}`} key={product.id}>
+                      {product.title}
+                    </Link>
+                  </Button>
+                ))}
+              <p>Monitors</p>
+              {products
+                .filter((product) => product.productType === 'monitor')
+                .map((product) => (
+                  <Button
+                    m='0'
+                    p='0 3rem'
+                    border='none'
+                    borderRadius='0'
+                    backgroundColor='rgba(255,255,255,1)'
+                    _hover={{
+                      bg: 'rgba(0,0,0,1)',
+                      color: 'rgba(255,255,255,1)',
+                    }}
+                    onClick={closeMenu}
+                    fontSize='2.4rem'
+                    fontWeight='100'
+                    textTransform='uppercase'
+                  >
+                    <Link to={`/products/${product.handle}`} key={product.id}>
+                      {product.title}
+                    </Link>
+                  </Button>
+                ))}
             </VStack>
           </DrawerBody>
           <DrawerFooter textAlign='center'>
